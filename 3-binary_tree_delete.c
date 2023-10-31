45704b138a88418b704ccb9144d3c7fdcb5f455a
mugiirar <munenevincentmugiira@gmail.com>
2023-10-31 08:15:45 -0700

    tree del

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - deletes an entire binary tree
+ * @tree: pointer to root
+ * Return: nothing
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	free(tree);
+}
