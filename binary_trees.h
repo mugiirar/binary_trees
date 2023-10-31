509b8d1eb20af801bd0c70d384a9059b6e2c6769
mugiirar <munenevincentmugiira@gmail.com>
2023-10-31 01:14:34 -0700

    h file

--- binary_trees.h
+++ binary_trees.h
@@ -30,4 +30,4 @@ typedef struct binary_tree_s heap_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
-#endif /* _BINARY_TREES_H_ */
+#endif
